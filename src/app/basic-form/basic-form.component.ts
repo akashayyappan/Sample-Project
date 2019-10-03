9b424b0649f976c561eda958172519e9b00b4533
akashayyappan <48151852+akashayyappan@users.noreply.github.com>
2019-10-03 21:21:09 +0530

    added source code

---
+++ src/app/basic-form/basic-form.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.css']
+})
+export class BasicFormComponent implements OnInit {
+
+  myForm:FormGroup;
+
+  public formHeight:any;
+  dropdownList: { item_id: number; item_text: string; }[];
+  dropdownSettings:IDropdownSettings;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+
+    this.myForm = this.fb.group({
+      "sample1": ["",[Validators.required]]
+    })
+
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Chennai' },
+      { item_id: 2, item_text: 'Bangaluru' },
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Navsari' },
+      { item_id: 5, item_text: 'New Delhi' },
+      { item_id: 6, item_text: 'Mumbai' }
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      enableCheckAll: false,
+      searchPlaceholderText: "Ex: Chennai",
+      itemsShowLimit: 4,
+      allowSearchFilter: true
+    };
+
+    let element = document.getElementsByClassName("form-content")[0];
+    let height = element.scrollHeight;
+    if(height < 200) { height = 240 }
+    this.formHeight = {'width':'100%','height':height +'px'};
+    let mq = window.matchMedia("(max-width: 800px")
+    if(mq.matches){
+      this.formHeight = {'width':'100%','height':height - 40 +'px'};
+    }
+  }
+
+}
